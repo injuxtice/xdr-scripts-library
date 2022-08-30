9a1c4562a172a9beb2232e5ccf790a395820a555
injuxtice <112502129+injuxtice@users.noreply.github.com>
2022-08-30 17:19:14 +0100

    Create get-file-hash.py

---
+++ get-file-hash.py
@@ -0,0 +1,19 @@
+import os
+import hashlib
+
+def run(path):
+    path = os.path.expanduser(path)
+    path = os.path.expandvars(path)
+    if os.path.isabs(path):
+        final_output = md5(path)
+        return {
+            "File_hash": final_output
+        }
+
+
+def md5(file_path):
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return(hash_md5.hexdigest())
