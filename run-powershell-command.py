a514b9dbc548d66ceb96f5b7c2ed7ab4e26e062a
injuxtice <injuxtice@pm.me>
2022-09-20 09:42:59 +0100

    Update run-powershell-command.py

--- run-powershell-command.py
+++ run-powershell-command.py
@@ -1,8 +1,10 @@
 import subprocess
 
 
-def run(commands_list):
+def run(command):
 	result = {}
-	for command in commands_list:
-		result = subprocess.run(["powershell", "-Command", command], capture_output=True)
-	return result
+	result = subprocess.run(["powershell", "-Command", command], capture_output=True)
+	return result.stdout.decode('utf-8').splitlines()
+
+new_command = run("ipconfig")
+print(new_command)
